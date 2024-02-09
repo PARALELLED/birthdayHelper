aa4aff99ed2f76256ac64b1a8774e52391a77db1
PARALELL <zx754615@gmail.com>
2024-02-09 22:08:09 +0800

    2024.22.08

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from linebot.models import *
 from message import *
 from new import *
 from Function import *
+from main1 import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
@@ -49,31 +50,40 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    today = check_birthday()
+    job()
+    msg = event.message.text
+    if msg != "":
+        line_bot_api.reply_message(event.reply_token, today)
+    else:
+        line_bot_api.reply_message(event.reply_token, "錯誤")
+
+
 
     #line_bot_api.push_message('U2ca83997b14c3819109e9dc4e161f3be', TextSendMessage(text=str("測試")))
 
-    msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
+    
+    # if '最新合作廠商' in msg:
+    #     message = imagemap_message()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '最新活動訊息' in msg:
+    #     message = buttons_message()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '註冊會員' in msg:
+    #     message = Confirm_Template()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '旋轉木馬' in msg:
+    #     message = Carousel_Template()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '圖片畫廊' in msg:
+    #     message = test()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '功能列表' in msg:
+    #     message = function_list()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # else:
+    #     message = TextSendMessage(text=msg)
+    #     line_bot_api.reply_message(event.reply_token, message)
 
 @handler.add(PostbackEvent)
 def handle_message(event):
---
+++ main1.py
@@ -0,0 +1,36 @@
+import datetime
+import schedule
+import time
+from plyer import notification
+
+# 定義生日日期
+birthday = {
+    "吳佳晏":"07-08",
+    "藍玉彤":"01-08",
+    "John": "05-20",
+    "Emma": "08-15",
+    "test1":"02-09",
+    # 添加更多的生日...
+}
+
+def check_birthday():
+    print("check_birthday start")
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    today = today[5:]
+    for name, bday in birthday.items():
+        if today == bday:
+            message = f"今天是{name}的生日！"
+            # notification.notify(
+            #     title="生日提醒",
+            #     message=message,
+            #     timeout=10
+            # )
+    return today
+
+def job():
+    # 每天 00:00 執行 (24小時)
+    schedule.every().day.at("22:05").do(check_birthday)
+    #print("開始執行")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)  # 每隔 60 秒檢查一次

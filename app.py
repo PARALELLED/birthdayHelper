e1e603a6ab43b1eae6b5939f7b5614b0550b97f6
PARALELL <zx754615@gmail.com>
2024-02-09 22:50:03 +0800

    10.49

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     today = check_birthday()
-    #job()
+    job()
     msg = event.message.text
     if msg != "":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=today))
